a1a5e3bb9cc798cee11aad785308804e1d2c9e8f
happyhun <younghun.ai@gmail.com>
2023-11-14 22:25:00 +0900

    feat: 예상 방문 날짜 검증

---
+++ src/main/java/validation/InputValidator.java
@@ -0,0 +1,19 @@
+package validation;
+
+public class InputValidator {
+
+    public static int getValidDate(String input) {
+        int date;
+        try {
+            date = Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        }
+
+        if (date < 1 || date > 31) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        }
+
+        return date;
+    }
+}
---
+++ src/test/java/validation/InputValidatorTest.java
@@ -0,0 +1,18 @@
+package validation;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class InputValidatorTest {
+
+    @ParameterizedTest
+    @ValueSource(strings = {"1일", "1.0", "-1", "0", "32"})
+    void 날짜_예외_테스트(String input) {
+        assertThatThrownBy(() -> InputValidator.getValidDate(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+    }
+
+}
