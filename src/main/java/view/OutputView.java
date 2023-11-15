9ba96a754241ffe82934f363050ca00c5bf3d013
happyhun <younghun.ai@gmail.com>
2023-11-15 20:12:51 +0900

    feat: 안내 문구 출력

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -82,4 +82,12 @@ public class OutputView {
         }
     }
 
+    public void printWelcomeMessage() {
+        System.out.println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
+    }
+
+    public void printPreviewMessage() {
+        System.out.println("12월 26일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!");
+        System.out.println();
+    }
 }
