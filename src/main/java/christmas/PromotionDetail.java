dcd30d1dff7c1b5a37c9f3773b9b9dfadbb991fb
happyhun <younghun.ai@gmail.com>
2023-11-15 16:30:58 +0900

    feat: 혜택 내역 구하기

---
+++ src/main/java/christmas/PromotionDetail.java
@@ -0,0 +1,52 @@
+package christmas;
+
+import christmas.menu.Menu;
+import christmas.menu.MenuType;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class PromotionDetail {
+
+    PromotionManager promotionManager = new PromotionManager();
+    OrderCalculator orderCalculator = new OrderCalculator();
+
+    public Map<Promotion, Integer> getPromotionDetails(Map<Menu, Integer> orders, Map<Menu, Integer> giftMenus, List<Integer> specialDates, int date) {
+        Map<Promotion, Integer> promotionDetails = initPromotionDetails();
+
+        setDiscountsForTotalPrice(giftMenus, specialDates, date, promotionDetails);
+        setDiscountsForDayOfWeek(orders, date, promotionDetails);
+
+        return promotionDetails;
+    }
+
+    private Map<Promotion, Integer> initPromotionDetails() {
+        Map<Promotion, Integer> promotionDetails = new HashMap<>();
+        for (Promotion promotion : Promotion.values()) {
+            promotionDetails.put(promotion, 0);
+        }
+        return promotionDetails;
+    }
+
+    private void setDiscountsForTotalPrice(Map<Menu, Integer> giftMenus, List<Integer> specialDates, int date, Map<Promotion, Integer> promotionDetails) {
+        promotionDetails.put(Promotion.GIFT_EVENT, orderCalculator.calculateTotalPrice(giftMenus));
+        promotionDetails.put(Promotion.CHRISTMAS_DDAY_DISCOUNT, promotionManager.getChristmasDdayDiscountAmount(date));
+        promotionDetails.put(Promotion.SPECIAL_DISCOUNT, promotionManager.getSpecialDiscountAmount(specialDates, date));
+    }
+
+    private void setDiscountsForDayOfWeek(Map<Menu, Integer> orders, int date, Map<Promotion, Integer> promotionDetails) {
+        int weekdayDiscountAmount = 0;
+        int weekendDiscountAmount = 0;
+
+        for (Map.Entry<Menu, Integer> order : orders.entrySet()) {
+            MenuType menuType = order.getKey().getType();
+            int count = order.getValue();
+            weekdayDiscountAmount += promotionManager.getWeekdayDiscountAmount(menuType, date) * count;
+            weekendDiscountAmount += promotionManager.getWeekendDiscountAmount(menuType, date) * count;
+        }
+
+        promotionDetails.put(Promotion.WEEKDAY_DISCOUNT, weekdayDiscountAmount);
+        promotionDetails.put(Promotion.WEEKEND_DISCOUNT, weekendDiscountAmount);
+    }
+}
---
+++ src/test/java/christmas/PromotionDetailTest.java
@@ -0,0 +1,36 @@
+package christmas;
+
+import christmas.menu.Menu;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class PromotionDetailTest {
+
+    PromotionDetail promotionDetail = new PromotionDetail();
+
+    @Test
+    void 할인_내역을_구한다() {
+        Map<Menu, Integer> orders = new HashMap<>();
+        orders.put(Menu.T_BONE_STEAK, 1);
+        orders.put(Menu.BBQ_RIBS, 1);
+        orders.put(Menu.CHOCOLATE_CAKE, 2);
+        orders.put(Menu.COKE_ZERO, 1);
+
+        Map<Menu, Integer> giftMenus = new HashMap<>();
+        giftMenus.put(Menu.CHAMPAGNE, 1);
+
+        List<Integer> specialDates = List.of(3);
+        int date = 3;
+
+        Map<Promotion, Integer> promotionDetails = promotionDetail.getPromotionDetails(orders, giftMenus, specialDates, date);
+        assertThat(promotionDetails.get(Promotion.CHRISTMAS_DDAY_DISCOUNT)).isEqualTo(1200);
+        assertThat(promotionDetails.get(Promotion.WEEKDAY_DISCOUNT)).isEqualTo(4046);
+        assertThat(promotionDetails.get(Promotion.SPECIAL_DISCOUNT)).isEqualTo(1000);
+        assertThat(promotionDetails.get(Promotion.GIFT_EVENT)).isEqualTo(25000);
+    }
+}
