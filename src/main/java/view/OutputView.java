d720821586a863479e39affa7e4245d634f94deb
happyhun <younghun.ai@gmail.com>
2023-11-15 19:12:03 +0900

    feat: 할인 전 총주문 금액 출력

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -2,14 +2,24 @@ package view;
 
 import christmas.menu.Menu;
 
+import java.text.NumberFormat;
+import java.util.Locale;
 import java.util.Map;
 
 public class OutputView {
+
     public void printOrders(Map<Menu, Integer> orders) {
         System.out.println("<주문 메뉴>");
         for (Map.Entry<Menu, Integer> order : orders.entrySet()) {
             System.out.printf("%s %d개\n", order.getKey().getName(), order.getValue());
         }
         System.out.println();
+    }
+
+    public void printTotalPrice(int totalPrice) {
+        System.out.println("<할인 전 총주문 금액>");
+        NumberFormat formatter = NumberFormat.getNumberInstance(Locale.KOREA);
+        String formattedAmount = formatter.format(totalPrice);
+        System.out.println(formattedAmount + "원\n");
     }
 }
