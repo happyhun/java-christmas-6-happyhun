b03712540e18697b35fc6eca10dc3c9314dc882b
happyhun <younghun.ai@gmail.com>
2023-11-15 13:27:02 +0900

    feat: 주문 입력

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,7 +1,10 @@
 package view;
 
 import camp.nextstep.edu.missionutils.Console;
+import christmas.Menu;
 import validation.InputValidator;
+
+import java.util.Map;
 
 public class InputView {
 
@@ -18,5 +21,20 @@ public class InputView {
             }
         }
         return date;
+    }
+
+    public Map<Menu, Integer> readOrders() {
+        Map<Menu, Integer> orders;
+        while (true) {
+            System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
+            String input = Console.readLine();
+            try {
+                orders = InputValidator.getValidOrders(input);
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        return orders;
     }
 }
