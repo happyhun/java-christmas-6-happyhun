9e5bf891b0984c9ba5be73bedd4a80659d3284b4
happyhun <younghun.ai@gmail.com>
2023-11-15 15:37:28 +0900

    feat: 배지 enum 추가

---
+++ src/main/java/christmas/Badge.java
@@ -0,0 +1,23 @@
+package christmas;
+
+public enum Badge {
+    STAR("별", 5000),
+    TREE("트리", 10000),
+    SANTA("산타", 20000);
+
+    private final String name;
+    private final int threshold;
+
+    private Badge(String name, int threshold) {
+        this.name = name;
+        this.threshold = threshold;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getThreshold() {
+        return threshold;
+    }
+}
